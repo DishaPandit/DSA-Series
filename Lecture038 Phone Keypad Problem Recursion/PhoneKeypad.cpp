ad43b4e4f9581fee1b2510adef2f1cbdf59d6662
DISHA <71927399+DishaPandit@users.noreply.github.com>
2022-01-19 19:02:30 +0530

    Phone keypad problem

---
+++ Lecture038 Phone Keypad Problem Recursion/PhoneKeypad.cpp
@@ -0,0 +1,54 @@
+/*
+    https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+    algorithm is almost same as subsequences problem.
+    only extra step is that everytime we push a value in output, after calling solve function again, we pop it back
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+
+private:
+    void solve(string digits, string output, int index, vector<string>& ans, string mapping[])
+    {
+        //base case
+        if(index>=digits.length())
+        {
+            ans.push_back(output);
+            return;
+        }
+        
+        int number = digits[index]-'0';
+        string value = mapping[number];
+        
+        for(int i = 0; i<value.length();i++)
+        {
+            output.push_back(value[i]);
+            solve(digits,output,index+1,ans,mapping);
+            output.pop_back();      //backtracking
+        }
+        
+            
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        
+        vector<string> ans;
+        
+        if(digits.length()==0)
+            return ans;
+    
+        string output = "";
+        int index;
+        
+        string mapping[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        
+        solve(digits,output,index,ans,mapping);
+        
+        return ans;
+        
+    }
+};
