6797295497a664d837ee3b90761137dc76b80afd
DISHA <71927399+DishaPandit@users.noreply.github.com>
2022-01-21 22:42:50 +0530

    Adding homework solution

--- Lecture037 Recursion - Subsets  Subsequences of String/SubsequencesUsingBits.cpp
+++ Lecture037 Recursion - Subsets  Subsequences of String/SubsequencesUsingBits.cpp
@@ -1,1 +1,33 @@
 //using bits
+// not in lexicographical order
+
+#include<iostream>
+#include<string.h>
+#include<math.h>
+using namespace std;
+
+void powerSet(string &str)
+{
+    int n = str.length();
+    int powSize = pow(2,n);
+
+    for(int counter =0; counter<powSize; counter++)
+    {
+        for(int j =0; j<n; j++)
+        {
+            if((counter & (1<<j)) != 0)
+                cout << str[j];
+        }
+        cout <<" ";
+    }
+}
+
+int main()
+{
+   string str; 
+   cout <<"Enter a string"<<endl;
+   cin>>str;
+
+   powerSet(str);
+
+}
--- Lecture037 Recursion - Subsets  Subsequences of String/SubsetUsingBits.cpp
+++ Lecture037 Recursion - Subsets  Subsequences of String/SubsetUsingBits.cpp
@@ -1,1 +1,34 @@
 //using bits
+// TC - Theta((2^n)*n)
+
+#include<iostream>
+#include<vector>
+#include<math.h>
+using namespace std;
+
+void powerSet(vector<int>& v)
+{
+    int n = v.size();
+    int powSize = pow(2,n);
+
+    for(int counter =0; counter<powSize; counter++)
+    {
+        for(int j =0; j<n; j++)
+        {
+            if((counter & (1<<j)) != 0)
+                cout << v[j];       
+        }
+        cout <<" ";
+    }
+}
+
+int main()
+{
+   vector<int> v;
+   v.push_back(1);
+   v.push_back(2);
+   v.push_back(3);
+
+   powerSet(v);
+
+}
