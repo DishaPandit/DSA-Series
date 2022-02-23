841b21d69819ccb9a908d3c5ada123a4584f7fda
DISHA <71927399+DishaPandit@users.noreply.github.com>
2022-02-23 23:45:30 +0530

    Adding handwritten notes

--- Lecture007 LeetCode Problem Solving Session/PowerOfTwo.cpp
+++ Lecture007 LeetCode Problem Solving Session/PowerOfTwo.cpp
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/power-of-two/
+//https://leetcode.com/problems/power-of-two/
 
 #include<iostream>
 #include<math.h>
@@ -21,12 +21,14 @@ public:
 */
 
 
-//optimised approach
+//optimised approach :isme hm har step pe jo answe nikaal rhe hai uska use krte ja rhe hai taki processing kam ho
 
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
         
+        //isme hm har step pe jo answe nikaal rhe hai uska use krte ja rhe hai taki processing kam ho
+        //isliye ans ko 1 liya hai aur neeche ans ko bs 2 se multiply krre hai
         int ans = 1;
         
         for(int i = 0; i <= 30; i++) {
